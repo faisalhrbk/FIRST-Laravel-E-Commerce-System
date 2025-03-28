70970dcd1dd07896335895a8ec2b652d01b1a5a7
faisalhrbk <2023-kiu-bs-3511@students.kiu.edu.pk>
2025-03-29 01:14:14 +0500

    use the protected variable

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -1,10 +1,12 @@
 <?php
-
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class Admin extends Model
+class Admin extends Authenticatable
 {
-    //
+    use Notifiable;
+
+    protected $fillable = ['name', 'email', 'password'];
 }
--- app/Models/Vendor.php
+++ app/Models/Vendor.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class Vendor extends Model
+class Vendor extends Authenticatable
 {
-    //
+    use Notifiable;
+
+    protected $fillable = ['name', 'email', 'password'];
 }
