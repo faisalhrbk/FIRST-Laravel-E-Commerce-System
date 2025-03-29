a604d6bd99f72c556509588951b610528edc3ea3
faisalhrbk <2023-kiu-bs-3511@students.kiu.edu.pk>
2025-03-29 15:58:40 +0500

    deleted the plural named conrollers and created singular named controllers

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class VendorsController extends Controller
+class VendorController extends Controller
 {
     /**
      * Display a listing of the resource.
