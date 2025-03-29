c8d4d6a5afc58627882700d60d257218eb88824d
faisalhrbk <2023-kiu-bs-3511@students.kiu.edu.pk>
2025-03-29 16:44:18 +0500

    created route now ill fix the html routes and web pages

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\VendorController;
+use App\Http\Controllers\CustomerController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+// Resource Controllers
+Route::resource('customer', CustomerController::class);
+Route::resource('vendor', VendorController::class);
+Route::resource('admin', AdminController::class);
+
+//Auth Controller
+Route::controller(AuthController::class)->group(function () {
+    Route::get('login/{role}', 'login')->name('login.{role}');
+    Route::post('login/{role}', 'processLogin')->name('login.process.{role}');
+
+    Route::get('register/{role}', 'register')->name('register.{role}');
+    Route::post('register/{role}', 'processRegister')->name('register.process.{role}');
 });
