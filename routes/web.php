08a9815ff6c42ae0e1e14b302b69782ac2bd118c
faisalhrbk <2023-kiu-bs-3511@students.kiu.edu.pk>
2025-04-03 16:46:54 +0500

    i was so horny i fuc#ked upp myselfff bro delated hlaf of files which took me 2 days t ocreate

--- routes/web.php
+++ routes/web.php
@@ -2,40 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\VendorController;
-use App\Http\Controllers\CustomerController;
-use App\Http\Controllers\AdminAuthController;
-use App\Http\Controllers\VendorAuthController;
-use App\Http\Controllers\CustomerAuthController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
-// Customer Auth controller
-Route::prefix('customer')->controller(CustomerAuthController::class)->group(function () {
-    Route::get('login', 'login')->name('customer.login');
-    Route::post('login', 'loginPost')->name('customer.login.post');
-    Route::get('register', 'register')->name('customer.register');
-    Route::post('register', 'registerPost')->name('customer.register.post');
-});
-Route::resource('customer', CustomerController::class);
 
-// VENDOR ROUTES
-Route::prefix('vendor')->controller(VendorAuthController::class)->group(function () {
-    Route::get('login', 'login')->name('vendor.login');
-    Route::post('login', 'loginPost')->name('vendor.login.post');
-    Route::get('register', 'register')->name('vendor.register');
-    Route::post('register', 'registerPost')->name('vendor.register.post');
-});
-Route::resource('vendor', VendorController::class);
-
-// ADMIN ROUTES
-Route::prefix('admin')->controller(AdminAuthController::class)->group(function () {
-    Route::get('login', 'login')->name('admin.login');
-    Route::post('login', 'loginPost')->name('admin.login.post');
-});
-Route::resource('admin', AdminController::class);
 
 Route::prefix('{role}')->controller(AuthController::class)->group(function () {
     Route::get('login', 'login')->name('login');
